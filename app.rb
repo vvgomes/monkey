9b6dccca7727888e7d4cc762ace1ce6d46fc01f4
Vinicius Gomes <vvgomess@gmail.com>
2012-09-04 17:40:56 -0400

    adds mongo

--- app.rb
+++ app.rb
@@ -2,15 +2,40 @@ require 'rubygems'
 require 'bundler'
 Bundler.require :default
 
+Mongoid.configure do |config|
+  if ENV['MONGOHQ_URL']
+    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+    uri = URI.parse(ENV['MONGOHQ_URL'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
+  else
+    begin
+      config.master = Mongo::Connection.
+        from_uri('mongodb://127.0.0.1:27017').
+        db('semprenalista')
+    rescue
+      puts 'No DB.'
+    end
+  end
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  payload = params['payload']
-  puts ">>>>>>>>>> FOO: #{payload['status_message']}"
-  puts ">>>>>>>>>> FOO: #{payload['commit']}"
-  puts ">>>>>>>>>> FOO: #{payload['brunch']}"
-
+  raw = params['payload']
+  Payload.create(
+    :status_message => raw['status_message'], 
+    :commit => raw['commit'],
+    :branch => raw['branch'])
   'done'
+end
+
+class Payload
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :status_message, :type => String
+  field :commit, :type => String
+  field :branch, :type => String
 end
