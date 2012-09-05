9c39dc4bcc7e8e12cc34f561fbbba05b985acb42
Vinicius Gomes <vvgomess@gmail.com>
2012-09-05 11:37:46 -0400

    no mongo needed

--- app.rb
+++ app.rb
@@ -2,41 +2,22 @@ require 'rubygems'
 require 'bundler'
 Bundler.require :default
 
-Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    config.master = conn.db(uri.path.gsub(/^\//, ''))
-  else
-    begin
-      config.master = Mongo::Connection.
-        from_uri('mongodb://127.0.0.1:27017').
-        db('semprenalista')
-    rescue
-      puts 'No DB.'
-    end
-  end
-end
+@@last = {
+  :status => "don't know", 
+  :commit => "wasn't watching it",
+  :branch => "master maybe?"
+}
 
 get '/' do
-  erb :index, :locals => { :last => Payload.last, :raw => @@raw }
+  erb :index
 end
 
 post '/' do
   raw = JSON.parse(params[:payload])
-  Payload.create(
-    :status_message => raw['status_message'], 
+  @@last = {
+    :status => raw['status_message'], 
     :commit => raw['commit'],
-    :branch => raw['branch'])
-  @@raw = raw
+    :branch => raw['branch']
+  }
   'done'
-end
-
-class Payload
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  field :status_message, :type => String
-  field :commit, :type => String
-  field :branch, :type => String
 end
