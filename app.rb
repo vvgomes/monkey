64c879890be7140107ef3fd49eb886c45113beb3
Vinicius Gomes <vvgomess@gmail.com>
2012-09-05 13:03:33 -0400

    deploy process

--- app.rb
+++ app.rb
@@ -19,5 +19,20 @@ post '/' do
     :commit => raw['commit'],
     :branch => raw['branch']
   }
-  'done'
+  deploy @@last[:status] == 'Passed'
+end
+
+def deploy
+  begin
+    `git clone --depth=100 --quiet git://github.com/vvgomes/victorykit.git` if Dir['victorykit'].empty?
+    `cd victorykit`
+    `git pull`
+    `git checkout -qf #{@@last[:commit]}`
+    `git remote add testserver git@heroku.com:still-retreat-5611.git`
+    `git push testserver #{@@last[:branch]}`
+  rescue => e
+    puts "Bad news: #{e}"
+    return 1
+  end  
+  0
 end
